082b387416e3c1e6316ea727c6c3b39e482fb49f
Christoph Hagen <github@christophhagen.de>
2024-03-05 11:02:48 +0100

    Add health extensions package

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.14.1"),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.25.2"),
+        .package(url: "https://github.com/christophhagen/HealthKitExtensions", from: "0.1.1"),
     ],
     targets: [
         .target(
@@ -20,6 +21,7 @@ let package = Package(
         dependencies: [
             .product(name: "SQLite", package: "sqlite.swift"),
             .product(name: "SwiftProtobuf", package: "swift-protobuf"),
+            .product(name: "HealthKitExtensions", package: "HealthKitExtensions")
         ]),
         .testTarget(
             name: "HKDatabaseTests",
