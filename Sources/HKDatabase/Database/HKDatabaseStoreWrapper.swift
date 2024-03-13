33e3a8f99c6f8074bc10e79375cf50a0d5c3c2f2
Christoph Hagen <github@christophhagen.de>
2024-03-13 16:07:39 +0100

    Improve health interface

--- Sources/HKDatabase/Database/HKDatabaseStoreWrapper.swift
+++ Sources/HKDatabase/Database/HKDatabaseStoreWrapper.swift
@@ -54,29 +54,58 @@ extension HKDatabaseStoreWrapper: HKHealthStoreInterface {
 
     // MARK: - Querying HealthKit data
 
-    public func read<T>(predicate: NSPredicate?, sortDescriptors: [SortDescriptor<HKQuantitySample>], limit: Int?) throws -> [T] where T : HKQuantitySampleContainer {
+    /**
+     Access category samples in a date interval.
+     - Parameter type: The category sample type
+     - Parameter start: The beginning of the date range of interest
+     - Parameter end: The end of the date range
+     */
+    public func samples<T>(ofType type: T.Type = T.self, from start: Date = .distantPast, to end: Date = .distantFuture) throws -> [T] where T: HKCategorySampleContainer {
+        try store.samples(category: T.categoryTypeIdentifier, from: start, to: end)
+            .map { T.init(categorySample: $0) }
+    }
 
-        try store.quantitySamples(type: T.quantityTypeIdentifier)
+    /**
+     Access category samples in a date interval.
+     - Parameter type: The category sample type
+     - Parameter predicate: The predicate to filter the samples
+     - Parameter sortDescriptors: The descriptors to order the returned samples
+     - Parameter limit: The maximum number of samples to return
+     */
+    public func samples<T>(ofType type: T.Type = T.self, predicate: NSPredicate? = nil, sortDescriptors: [SortDescriptor<HKCategorySample>] = [], limit: Int? = nil) throws -> [T] where T : HKCategorySampleContainer {
+        try store.samples(category: T.categoryTypeIdentifier)
+            .filtered(using: predicate)
+            .sorted(using: sortDescriptors)
+            .limited(by: limit)
+            .map(T.init(categorySample:))
+    }
+
+    /**
+     Access quantity samples in a date interval.
+     - Parameter type: The quantity sample type
+     - Parameter start: The beginning of the date range of interest
+     - Parameter end: The end of the date range
+     */
+    public func samples<T>(ofType type: T.Type = T.self, from start: Date = .distantPast, to end: Date = .distantFuture) throws -> [T] where T: HKQuantitySampleContainer {
+        try store.samples(quantity: T.quantityTypeIdentifier, from: start, to: end, unit: T.defaultUnit)
+            .map { T.init(quantitySample: $0) }
+    }
+
+    public func samples<T>(ofType type: T.Type = T.self, predicate: NSPredicate?, sortDescriptors: [SortDescriptor<HKQuantitySample>], limit: Int?) throws -> [T] where T : HKQuantitySampleContainer {
+
+        try store.samples(quantity: T.quantityTypeIdentifier)
             .filtered(using: predicate)
             .sorted(using: sortDescriptors)
             .limited(by: limit)
             .map(T.init(quantitySample:))
     }
 
-    public func read<T>(predicate: NSPredicate?, sortDescriptors: [SortDescriptor<HKCorrelation>], limit: Int?) throws -> [T] where T : HKCorrelationContainer {
+    public func samples<T>(ofType type: T.Type = T.self, predicate: NSPredicate?, sortDescriptors: [SortDescriptor<HKCorrelation>], limit: Int?) throws -> [T] where T : HKCorrelationContainer {
         try store.correlationSamples(type: T.correlationType)
             .filtered(using: predicate)
             .sorted(using: sortDescriptors)
             .limited(by: limit)
             .map(T.init(correlation:))
-    }
-
-    public func read<T>(predicate: NSPredicate?, sortDescriptors: [SortDescriptor<HKCategorySample>], limit: Int?) throws -> [T] where T : HKCategorySampleContainer {
-        try store.categorySamples(type: T.categoryTypeIdentifier)
-            .filtered(using: predicate)
-            .sorted(using: sortDescriptors)
-            .limited(by: limit)
-            .map(T.init(categorySample:))
     }
 
     // MARK: - Reading characteristic data
