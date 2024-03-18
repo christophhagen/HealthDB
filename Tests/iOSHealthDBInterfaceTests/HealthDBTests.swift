3a24c0a94b10b230a0a68be3af54a1a31631bb8d
Christoph Hagen <github@christophhagen.de>
2024-03-18 11:15:57 +0100

    Rename tests

---
+++ Tests/iOSHealthDBInterfaceTests/HealthDBTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import HealthDB
+import HealthKit
+import HealthKitExtensions
+import CoreLocation
+
+final class HealthDBTests: XCTestCase {
+
+    func testNothing() async {
+    }
+}
