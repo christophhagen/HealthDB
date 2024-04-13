13b469cccd57c6b26a1be5f13e6c5954c4bb1395
Christoph Hagen <github@christophhagen.de>
2024-04-13 11:01:00 +0200

    Add DocC dependency for documentation

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
         .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.14.1"),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.25.2"),
         .package(url: "https://github.com/christophhagen/HealthKitExtensions", from: "0.4.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(
@@ -21,7 +22,11 @@ let package = Package(
         dependencies: [
             .product(name: "SQLite", package: "sqlite.swift"),
             .product(name: "SwiftProtobuf", package: "swift-protobuf"),
-            .product(name: "HealthKitExtensions", package: "HealthKitExtensions")
+            .product(name: "HealthKitExtensions", package: "HealthKitExtensions"),
+        ],
+        exclude: [
+            "Model/ECG/ECGVoltageData.proto",
+            "Model/Workouts/EventMetadata.proto",
         ]),
         .testTarget(
             name: "HKDatabaseTests",
